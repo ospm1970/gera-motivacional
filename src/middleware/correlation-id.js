8d6f790e5f99a7c9220f3ab80c4dee746c7b83ec
Pipeline Executor <pipeline@executor.local>
2026-04-20 17:58:09 -0300

    feat: Alterar a tela do projeto para depois da geração das frases exista um ca

---
+++ src/middleware/correlation-id.js
@@ -0,0 +1,8 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export function correlationIdMiddleware(req, res, next) {
+  const correlationId = req.headers['x-correlation-id'] || uuidv4();
+  req.headers['x-correlation-id'] = correlationId;
+  res.setHeader('X-Correlation-ID', correlationId);
+  next();
+}
